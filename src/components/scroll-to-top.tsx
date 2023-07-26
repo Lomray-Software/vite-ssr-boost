958e9739a77cebb90e807fbfca714e6e511840fb
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-07-26 18:38:08 +0200

    feat: scroll to top reset page on reload option

--- src/components/scroll-to-top.tsx
+++ src/components/scroll-to-top.tsx
@@ -1,15 +1,25 @@
 import type { FC } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+
+interface IScrollToTop {
+  shouldReloadReset?: boolean;
+}
 
 /**
  * Scroll page to top on every pathname (url) change
  * @constructor
  */
-const ScrollToTop: FC = () => {
+const ScrollToTop: FC<IScrollToTop> = ({ shouldReloadReset = false }) => {
   const { pathname } = useLocation();
+  const prev = useRef(pathname);
 
   useEffect(() => {
+    if (!shouldReloadReset && prev.current === pathname) {
+      return;
+    }
+
+    prev.current = pathname;
     window.scrollTo(0, 0);
   }, [pathname]);
 
